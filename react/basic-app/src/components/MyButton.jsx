0b760f34f3afc4fe643e1afc978c04034f1d323a
unknown <dbsgywls667@gmail.com>
2025-09-08 19:42:04 +0900

    파일 생성

---
+++ react/basic-app/src/components/MyButton.jsx
@@ -0,0 +1,8 @@
+
+export function MyButton({name, type, style}) {  //props = {name:"회원가입"}
+    return (
+        <button type={type}
+                className={style}
+                >{name}</button>
+    );
+}
