bbf71ebd7c2cfb65d297fa051c28e35feb205950
bale <>
2025-09-25 16:40:39 +0900

    파일 추가

---
+++ react/shoppy-app-hooks/src/components/commons/Header.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import { FaShoppingBasket } from "react-icons/fa";
+import { useState, useEffect, useContext } from "react";
+import { CartContext } from "../../context/CartContext";
+export function Header() {
+    const {cartCount} = useContext(CartContext);
+    useEffect(() => {
+        const loginInfo = localStorage.getItem('loginInfo');
+
+        // if(loginInfo) {
+            console.log('loginInfo', JSON.parse(loginInfo) );
+        // } else {
+        //     console.log("저장된 로그인 정보 없음");
+            
+        // }
+    },[]);
+    
+    return (
+        <div className="header-outer">
+            <div className="header">
+                <Link to="/" className="header-left"><FaShoppingBasket /><span>Shoppy-hooks</span></Link>
+                <nav className="header-right">
+                    <Link to="/all" >Products</Link>
+                    <Link to="/cart" >MyCart({cartCount})</Link>
+                    <Link to="/login" ><button type="button">Login</button></Link>
+                    <Link to="/signup" ><button type="button">Sign up</button></Link>
+                    <Link to="/support" ><button type="button">고객센터</button></Link>
+                </nav>
+            </div>
+        </div>
+    );
+}
+
+
